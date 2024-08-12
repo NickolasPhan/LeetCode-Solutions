b7767096e6987f85a33f3f6a5d7474411ec47fe2
NickolasPhan <nickvp55@gmail.com>
2024-08-12 09:15:45 -0400

    WIP new solution (find two duplicates)

---
+++ LongestPalindromeSubString/attempt2_LongestPalindrome.py
@@ -0,0 +1,40 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        print("Evaluating:", s)
+        testString = ""
+        duplicates = []
+        i = 0
+
+        # print(s[1:])
+        # print(s[2:].index("b"))
+
+        # print(indices)
+
+        # it looks for two letters and evaluates whatever is between them
+
+        for char in s:
+            if char in testString and len(testString) > 1:
+                if char not in duplicates:
+                    duplicates.append(char)
+            
+            testString += char
+
+        for dupChar in duplicates:
+            indices = [i for i, x in enumerate(duplicates) if x == dupChar]
+            for index in indices:
+                # if s[]
+                pass
+        print(duplicates)
+        print(testString)
+
+def main():
+    # s = "Hello World!"
+    # s = input("Input test string:")
+    s = "sbabad"
+    solution = Solution()
+    solution.longestPalindrome(s)
+
+if __name__ == "__main__":
+    print()
+    main()
+    print()
