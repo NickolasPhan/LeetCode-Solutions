b3cdc7967c1d9e891f89f3aaaf78898fce5c2878
NickolasPhan <nickvp55@gmail.com>
2024-08-12 01:19:16 -0400

    Longest Palindrome solution submitted but computation time is too long; trying to find a better way

--- LongestPalindromeSubString/LongestPalindrome.py
+++ LongestPalindromeSubString/LongestPalindrome.py
@@ -1,20 +1,39 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
         print("Evaluating:", s)
-        index = 0
+        testString = ""
+        resStrings = []
+        i = 0
+        j = 0
 
-        for char in s:
-            if s[index] == s[index+1]:
-                pass
+        # it looks for two letters and evaluates whatever is between them
 
-        # print(s[::-1])
+        while i < len(s):
+            j = i
+
+            while j < len(s):
+                testString += s[j]
+
+                if testString == testString[::-1]:
+                    resStrings.append(testString)
+
+                j += 1
+            testString = ""
+
+            i += 1
+        
+        try:
+            bigString = max(resStrings, key=len)
+        except ValueError:
+            return []
+        else:
+            return bigString
 
 def main():
     # s = "Hello World!"
-    s = "babad"
-    # s = input("Input test string:")
+    s = input("Input test string:")
     solution = Solution()
-    solution.longestPalindrome(s)
+    print(solution.longestPalindrome(s))
 
 if __name__ == "__main__":
     print()
