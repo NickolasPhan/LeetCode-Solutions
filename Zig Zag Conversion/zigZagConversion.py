915df8947382f4d270cc72838ebad828c7db5e36
NickolasPhan <67134735+NickolasPhan@users.noreply.github.com>
2025-01-09 18:03:29 -0500

    first finished attempt

--- Zig Zag Conversion/zigZagConversion.py
+++ Zig Zag Conversion/zigZagConversion.py
@@ -1,29 +1,63 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
-        print(s)
-
         all_list = []
         curr_list = []
         initArr = ['' for i in range(numRows)]
         numZags = numRows - 2
+        zagFlag = False
+        positionCounter = 0
         # working on - how to know when the loop is in the zag section? how to navigate that?
 
-        for idx, char in enumerate(s):
-            curr_list.append(char)
+        for char in s:
+            if zagFlag:
+                zagArr = initArr[:]
+                zagArr[numZags] = char
+                all_list.append(zagArr)
+                numZags -= 1
 
-            if (idx+1)%numRows==0:
+                if numZags == 0:
+                    zagFlag = not zagFlag
+                    numZags = numRows-2
+
+                continue
+            else:
+                curr_list.append(char)
+                positionCounter += 1
+
+            if positionCounter%numRows==0:
                 all_list.append(curr_list)
                 curr_list = []
+                zagFlag = not zagFlag
+        else:
+            if curr_list:
+                if len(curr_list) != numRows:
+                    curr_list.extend([""] * (numRows - len(curr_list)))
+                all_list.append(curr_list)
 
-        print(initArr)
+        result = self.createZigZag(lst=all_list, numRows=numRows)
+        return result
+
+    def createZigZag(self, lst: list, numRows: int) -> str:
+        orderedLst = []
+        for idx in range(numRows):
+            chars = [sublist[idx] for sublist in lst if sublist]
+            orderedLst += chars
+            # print(' '.join(chars))
+
+        resultLst = [char for char in orderedLst if char]
+        resultStr = ''.join(resultLst)
+
+        return resultStr
 
 def main():
     var = Solution()
 
-    string = 'PAYPALISHIRING'
-    rows = 3
+    string = 'ABC'
+    rows = 1
 
-    var.convert(string, rows)
+    result = var.convert(string, rows)
+
+    print(result)
 
 if __name__ == "__main__":
     print()
