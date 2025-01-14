2b26bf2dd1eb211856cbad19ed3b371a96863bb1
NickolasPhan <67134735+NickolasPhan@users.noreply.github.com>
2025-01-14 14:47:57 -0500

    finish stringToInt (atoi) implementation

--- String to Integer (atoi)/stringToIntegerAtoi.py
+++ String to Integer (atoi)/stringToIntegerAtoi.py
@@ -1,10 +1,41 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
-        return s
+        res = ''
+        s = s.strip()
+
+        flag = False
+        try:
+            if s[0] == '-':
+                flag = True
+                s = s[1:]
+            elif s[0] == '+':
+                s = s[1:]
+        except IndexError:
+            return 0
+
+        for char in s:
+            if char.isnumeric():
+                res += char
+            else:
+                break
+
+        res = int(res) if res else 0
+        res = res*-1 if flag else res
+
+        if -2**31 > res:
+            res = -2**31
+        elif res > 2**31 - 1:
+            res = 2**31 - 1
+        
+        return res
+
 
 def main():
     var = Solution()
-    res = var.myAtoi(s="s")
+
+    string = "   -042"
+
+    res = var.myAtoi(s=string)
     print(res)
 
 if __name__ == "__main__":
