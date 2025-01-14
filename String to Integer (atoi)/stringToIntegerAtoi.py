b6396e20de7648375f72840cd12e0dcfa537ab7c
NickolasPhan <67134735+NickolasPhan@users.noreply.github.com>
2025-01-14 13:23:12 -0500

    init stringToInteger (Atoi)

---
+++ String to Integer (atoi)/stringToIntegerAtoi.py
@@ -0,0 +1,13 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        return s
+
+def main():
+    var = Solution()
+    res = var.myAtoi(s="s")
+    print(res)
+
+if __name__ == "__main__":
+    print()
+    main()
+    print()
