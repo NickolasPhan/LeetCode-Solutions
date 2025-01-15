8de7c80b269a0be0ade4d4ac4365ac3eba29b346
NickolasPhan <67134735+NickolasPhan@users.noreply.github.com>
2025-01-15 17:05:19 -0500

    working on naive solution

--- Minimize XOR/minimizeXOR.py
+++ Minimize XOR/minimizeXOR.py
@@ -1,13 +1,40 @@
 class Solution:
+    def getNumBits(self, num: int) -> int:
+        pass
+
     def minimizeXor(self, num1: int, num2: int) -> int:
-        return 0
+        print(f'num1: {num1:0>{num2.bit_length()}b}\nnum2: {num2:b}\n')
+        print(f'num1 bit length: {num1.bit_length()}\nnum2 bit length: {num2.bit_length()}\n')
+        # print(f'dec: {xor}\nbit: {xor:b}\nbit count: {xor.bit_count()}')
+
+        # naive solution : brute force
+
+        # solutionBank = []
+
+        i = num1+1
+        print(i)
+        while True:
+            if i.bit_count() != num2.bit_count():
+                i += 1
+                continue
+            else:
+                return i
+                # i += 1
+
 
 def main():
     var = Solution()
+    
+    num1 = 25
+    num2 = 72
 
-    res = Solution.minimizeXor(num1=0, num2=0)
+    res = var.minimizeXor(num1=num1, num2=num2)
 
-    print(res)
+    print(f'dec: {res}\nbin: {res:b}')
+
+
+    # for num in res:
+    #     print(f'{num:b}')
 
 if __name__ == "__main__":
     print()
