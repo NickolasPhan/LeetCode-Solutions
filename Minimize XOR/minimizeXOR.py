caecae314fbda7c51e64938ec3e9a2430445e49f
NickolasPhan <67134735+NickolasPhan@users.noreply.github.com>
2025-01-15 14:00:59 -0500

    init minimizeXOR

---
+++ Minimize XOR/minimizeXOR.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        return 0
+
+def main():
+    var = Solution()
+
+    res = Solution.minimizeXor(num1=0, num2=0)
+
+    print(res)
+
+if __name__ == "__main__":
+    print()
+    main()
+    print()
